a2de61f802ad89d8879429515754f2b7629c81df
Timothy Losper <tim_losper@yahoo.com>
2023-01-05 13:06:31 +0100

    Adding various pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from '@angular/material/dialog';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,17 @@ import { MatDialog } from '@angular/material/dialog';
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  constructor(public dialog: MatDialog) { }
-  // This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
+  // constructor(public dialog: MatDialog) { }
+  // // This is the function that will open the dialog when the signup button is clicked  
+  // openUserRegistrationDialog(): void {
+  //   this.dialog.open(UserRegistrationFormComponent, {
+  //     // Assigning the dialog a width
+  //     width: '280px'
+  //   });
+  // }
+  // openMoviesDialog(): void {
+  //   this.dialog.open(MovieCardComponent, {
+  //     width: '500px'
+  //   });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,47 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MovieGenreComponent } from './movie-genre/movie-genre.component';
+import { DirectorComponent } from './director/director.component';
+import { DescriptionComponent } from './description/description.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegistrationFormComponent
+    UserRegistrationFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
+    UserLoginFormComponent,
+    ProfileComponent,
+    MovieGenreComponent,
+    DirectorComponent,
+    DescriptionComponent,
+    NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
@@ -31,6 +60,7 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
     MatDialogModule,
     MatSnackBarModule,
     FormsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
